3cc672ef4b34aa971aada01d1bf5dca5a376e205
Ciobanu Laurentiu <ciobanulaur@gmail.com>
2023-11-08 14:32:56 +0200

    update latest

--- init.lua
+++ init.lua
@@ -63,6 +63,7 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
+    vim.opt.guifont = "FiraCode Nerd Font:h12"
     -- Set up custom filetypes
     -- vim.filetype.add {
     --   extension = {
